31ea6295bc718021ec1e74d1c413011238503bfe
Vivek Jha <vivek.ajay.p50@gmail.com>
2024-11-23 10:59:49 +0530

    appwrite Auth && Database && Storage services endpoints

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import { Account, Client } from "appwrite";
+import { Account, Client, ID } from "appwrite";
 import config from "../config/config";
 
 class AuthService {
@@ -10,6 +10,61 @@ class AuthService {
       .setEndpoint(config.appwriteEndpoint)
       .setProject(config.appwriteProjectId);
     this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, fullName = "" }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        fullName
+      );
+
+      if (userAccount) {
+        // Log In him.
+        return this.login({ email, password });
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error("Appwrite Service :: Create User :: Error :: ", error);
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const loggedInUser = await this.account.createEmailPasswordSession(
+        email,
+        password
+      );
+      if (!loggedInUser) {
+        return null;
+      }
+      return loggedInUser;
+    } catch (error) {
+      console.error("Appwrite Service :: Login User :: Error :: ", error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      const user = await this.account.get();
+      if (!user) {
+        return null;
+      }
+      return user;
+    } catch (error) {
+      console.error("Appwrite Service :: Get Current User :: Error :: ", error);
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.error("Appwrite Service :: Logout User :: Error :: ", error);
+    }
   }
 }
 
---
+++ src/appwrite/conf.js
@@ -0,0 +1,136 @@
+import { Client, Databases, ID, Query, Storage } from "appwrite";
+import config from "../config/config";
+
+class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteEndpoint)
+      .setProject(config.appwriteProjectId);
+
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createBlog({ title, content, slug, image, author, status }) {
+    try {
+      const response = await this.databases.createDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        ID.unique(),
+        { title, content, image, author, status }
+      );
+
+      return response;
+    } catch (error) {
+      console.error("Appwrite Service :: Create a Blog :: Error :: ", error);
+    }
+  }
+
+  async updateBlog(slug, data) {
+    try {
+      const response = await this.databases.updateDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug,
+        data
+      );
+      return response;
+    } catch (error) {
+      console.error("Appwrite Service :: Update a Blog :: Error :: ", error);
+    }
+  }
+
+  async deleteBlog(slug) {
+    try {
+      await this.databases.deleteDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.error("Appwrite Service :: Delete a Blog :: Error :: ", error);
+      return false;
+    }
+  }
+
+  async getSingleBlog(slug) {
+    try {
+      const response = await this.databases.getDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug
+      );
+      return response;
+    } catch (error) {
+      console.error("Appwrite Service :: Get Single Blog :: Error :: ", error);
+    }
+  }
+
+  async getAllBlogs(queries = [Query.equal("status", ["active"])]) {
+    try {
+      const response = await this.databases.listDocuments(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        queries
+      );
+      return response;
+    } catch (error) {
+      console.error("Appwrite Service :: Get all Blog :: Error :: ", error);
+      return false;
+    }
+  }
+
+  async uploadFile(file) {
+    try {
+      const response = await this.Storage(
+        config.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+      return response;
+    } catch (error) {
+      console.error("Appwrite Service :: Upload a File :: Error :: ", error);
+    }
+  }
+
+  async updateFile(fileId) {
+    try {
+      const response = await this.Storage.updateFile(
+        config.appwriteBucketId,
+        fileId
+      );
+      return response;
+    } catch (error) {
+      console.error("Appwrite Service :: File Preview :: Error :: ", error);
+    }
+  }
+
+  async getFilePreview(fileId) {
+    try {
+      await this.Storage.getFilePreview(config.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.error("Appwrite Service :: File Preview :: Error :: ", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.Storage(config.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.error("Appwrite Service :: Delete a File :: Error :: ", error);
+      return false;
+    }
+  }
+}
+
+const services = new Service();
+
+export default services;
